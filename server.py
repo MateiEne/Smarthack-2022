5c66bfde6f25ee62c7ceff80827c6ddba00b2d20
Alexandru Gheorghe <tccz658@gmail.com>
2022-11-05 23:59:29 +0200

    server sper ca merge

--- server.py
+++ server.py
@@ -1,23 +1,46 @@
 import socket
-import threading
+from _thread import *
+import sys
 
-# create a socket object
-serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server = "192.168.22.44"
+port = 5555
 
-# get local machine name
-host = socket.gethostname()
-port = 9999
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# bind to the port
-serversocket.bind((host, port))
+try:
+    s.bind((server, port))
+except socket.error as e:
+    str(e)
 
-# queue up to 5 requests
-serversocket.listen(5)
+s.listen(2)
+print("Waiting for a connection, Server Started")
+
+
+def threaded_client(conn):
+    conn.send(str.encode("Connected"))
+    reply = ""
+    while True:
+        try:
+            data = conn.recv(2048)
+            reply = data.decode("utf-8")
+
+            if not data:
+                print("Disconnected")
+                break
+            else:
+                print("Received: ", reply)
+                print("Sending : ", reply)
+
+            conn.sendall(str.encode(reply))
+        except:
+            break
+
+    print("Lost connection")
+    conn.close()
+
 
 while True:
-    # establish a connection
-    clientsocket,addr = serversocket.accept()
-    print("Got a connection from %s" % str(addr))
-    msg = 'Thank you for connecting'+ "\r\n"
-    clientsocket.send(msg.encode('ascii'))
-    clientsocket.close()
+    conn, addr = s.accept()
+    print("Connected to:", addr)
+
+    start_new_thread(threaded_client, (conn,))
