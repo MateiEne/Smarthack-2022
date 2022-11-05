b3003bd4469177a7768456ffd43ef6c6016b1696
Alexandru Gheorghe <tccz658@gmail.com>
2022-11-05 22:07:39 +0200

    PLS WORK

---
+++ farmer-app/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import React from 'react';
+import NavBar from './components/NavBar';
+import Body from './components/Body';
+
+function App() {
+  return (
+    <>
+      <NavBar />
+      <Body />
+    </>
+  );
+}
+
+export default App;
---
+++ farmer-app/src/components/Content.js
@@ -0,0 +1,14 @@
+import './Content.css'
+
+
+function Content() {
+    return (
+        <>
+            <div className="Central">
+                <p className='Message'>Vreau sa</p>
+            </div>
+        </>
+    );
+}
+
+export default Content;
